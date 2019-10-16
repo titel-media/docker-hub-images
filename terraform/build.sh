40cbe4eeda058d1b0429f0d2b2519b036f5c1df0
Hugo Duksis <duksis@gmail.com>
2019-10-16 12:28:05 +0200

    it's light and not latest

--- terraform/build.sh
+++ terraform/build.sh
@@ -1,4 +1,4 @@
 TERRAFORM_VERSION="$(cat Dockerfile-light | grep 'ENV TERRAFORM_VERSION=' | cut -d= -f2 | tr -d '\n')"
 
-docker build -t titelmedia/terraform:latest -f Dockerfile-light .
-docker tag titelmedia/terraform:latest "titelmedia/terraform:${TERRAFORM_VERSION:?}"
+docker build -t titelmedia/terraform:light -f Dockerfile-light .
+docker tag titelmedia/terraform:light "titelmedia/terraform:${TERRAFORM_VERSION:?}"
